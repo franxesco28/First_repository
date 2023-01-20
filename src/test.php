3554166d0c04f02ff379b4ce650bb72d3296a2ed
franxesco28 <119877754+franxesco28@users.noreply.github.com>
2023-01-20 12:18:44 +0100

    Update test.php

--- src/test.php
+++ src/test.php
@@ -1,9 +1,11 @@
+PRIMA PROVA
+- Scrivere un programma che trovi il maggiore tra 3 numeri
+<br> 
 <?php
 
  $a = 12;
  $b = 21;
  $c = 18;
- $result = $b > $c  ;
 ?>
 
 a = 12 <br>
@@ -11,17 +13,44 @@ b = 21 <br>
 c = 18 <br>
 <br>
 <?php
-    if ( $a > $c ){
-    echo "Il numero maggiore è : $result ";
-}
-    if ($result = $c > $b  ) {
-    echo  "Il numero maggiore è : $result ";
-    }
 
-    if ($result = $b > $c && $b > $a) {
-    echo "Il numero maggiore  è :  $result" ;
-    }
+    if ( $a > $c && $c > $b ){
+    echo "Il numero maggiore è :  " . $c ;
+}
+    elseif ( $c > $b && $b < $a ) {
+    echo  "Il numero maggiore è : " . $a ;
+}
+    if ( $b > $c && $b > $a) {
+    echo "Il numero maggiore  è : " . $b ;
+}
 
 
 
 ?>
+<br>
+<br>
+
+//SECONDA PROVA
+- Scrivere un programma per trovare numeri pari o dispari
+<br> 
+<?php
+$x = 0;
+do {
+  echo "Numeri Pari: $x <br>";
+  $x+=2;
+} while ($x <= 30);
+?>
+
+<?php
+$x=1;
+
+do {
+  echo "<br> Numeri Dispari: $x ";
+   $x+=2;
+} while ($x < 30);
+?>
+
+</body>
+</html>
+?>
+
