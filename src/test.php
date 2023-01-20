b68f21d826864ff84acb716d964272af1492ccd2
franxesco28 <119877754+franxesco28@users.noreply.github.com>
2023-01-20 14:29:31 +0100

    Update test.php

--- src/test.php
+++ src/test.php
@@ -1,6 +1,6 @@
 PRIMA PROVA
 - Scrivere un programma che trovi il maggiore tra 3 numeri
-<br> 
+<br>
 <?php
 
  $a = 12;
@@ -30,9 +30,9 @@ c = 18 <br>
 <br>
 <br>
 
-//SECONDA PROVA
+SECONDA PROVA
 - Scrivere un programma per trovare numeri pari o dispari
-<br> 
+<br>
 <?php
 $x = 0;
 do {
@@ -49,8 +49,42 @@ do {
    $x+=2;
 } while ($x < 30);
 ?>
-
-</body>
-</html>
+<br>
+<br>
+TERZA PROVA
+- Creare uno script per stampare la riga 1-2-3-4-5-6-7-8-9-10 attraverso i loop...
+<br>
+<?php  $i =1 ?>
+<?php
+for ($i = 1; ($i <= 10); $i++){
+    echo $i ," " ;
+}
 ?>
+<br>
+<br>
+QUARTA PROVA
+- Creare uno script per sommare tutti gli interi tra 0 e 30 e infine mostrare il risultato
+<br>
+<br>
+<?php
 
+
+$totPari = (2+4+6+8+10+12+14+16+18+20+22+24+26+28+30);
+$totDisp = (1+3+5+7+9+11+13+15+17+19+21+23+25+27+29);
+
+    if ($totPari >= 230) {
+        echo  "Totale dei numeri pari : " . $totPari;
+    }
+    elseif ($totDisp >= 225) {
+        echo "Totale dei numeri dispari : " . $totDisp;
+    }
+?>
+<br>
+<br>
+QUINTA PROVA
+- Creare uno script che costruisca il seguente pattern...
+<br>
+<br>
+<?php
+
+?>
